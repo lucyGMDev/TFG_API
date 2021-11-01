fe6525c19688c453d91cf80853564e36ff0ed7e6
Lucia Izquierdo Delgado <lucygm.dev@gmail.com>
2021-11-01 23:52:09 +0100

    Class for user body created

---
+++ src/main/java/com/tfg/api/data/bodies/UserBody.java
@@ -0,0 +1,24 @@
+package com.tfg.api.data.bodies;
+
+public class UserBody {
+  private String email;
+
+  public UserBody() {
+  }
+
+  public UserBody(String email) {
+    this.email = email;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  
+ 
+  
+}
