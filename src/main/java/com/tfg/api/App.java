5d74b4a479aa40f9db9282c790c81f9d40888dc6
Lucia Izquierdo Delgado <lucygm.dev@gmail.com>
2021-11-01 20:48:39 +0100

    Project Started

---
+++ src/main/java/com/tfg/api/App.java
@@ -0,0 +1,40 @@
+package com.tfg.api;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.internal.ProcessingException;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public final class App {
+
+	private static final String BASE_URI = "http://192.168.1.7:8080/api/";
+
+	private static final HttpServer createConection() throws ProcessingException, URISyntaxException {
+		ResourceConfig rc = new ResourceConfig().packages("com.tfg.api.resources");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI),rc);
+		return server;
+	}
+
+	/**
+	 * Says hello to the world.
+	 * 
+	 * @param args The arguments of the program.
+	 */
+	public static void main(String[] args) {
+		try {
+
+			HttpServer server = createConection();
+			System.out.println(
+					String.format("Starting Server at addres %sapplication.wadl \nPress any key to shutdown ...", BASE_URI));
+			System.in.read();
+			server.shutdown();
+		} catch (Exception e) {
+			System.out.println("Hello");
+			e.printStackTrace();
+		}
+	}
+}
