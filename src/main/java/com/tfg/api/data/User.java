8fe0388e701df699965304d0cc39980246b57ef6
Lucia Izquierdo Delgado <lucygm.dev@gmail.com>
2021-11-01 23:53:31 +0100

    Class for user data implemented

---
+++ src/main/java/com/tfg/api/data/User.java
@@ -0,0 +1,63 @@
+package com.tfg.api.data;
+
+import java.util.Date;
+
+public class User {
+  private String email;
+  private String username;
+  private String name;
+  private String lastName;
+  private Date createdDate;
+
+  public User() {
+  }
+
+  public User(String email, String username, String name, String lastName, Date createdDate) {
+    this.email = email;
+    this.username = username;
+    this.name = name;
+    this.lastName = lastName;
+    this.createdDate = createdDate;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
+}
